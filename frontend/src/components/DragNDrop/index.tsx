7846fb58face90020c7f0fd97d37033de936a3dc
LeomaiaJr <leomaiajr@hotmail.com>
2023-08-01 15:11:24 -0300

    feat: add drag n drop component

---
+++ frontend/src/components/DragNDrop/index.tsx
@@ -0,0 +1,101 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import SnackbarUtils from '../../utils/SnackbarUtils';
+import {
+  CustomTypography,
+  DropArea,
+  FileDataWrapper,
+  IconWrapper,
+} from './styles';
+
+interface DragNDropProps {
+  setFile: (file: File | undefined) => void;
+  file?: File;
+}
+
+const DragNDrop = ({ setFile, file }: DragNDropProps) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+
+    if (file.name.endsWith('.csv')) setFile(file);
+    else
+      SnackbarUtils.error(
+        'Invalid file extension, only .csv files are accepted'
+      );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <>
+      {file === undefined ? (
+        <DropArea active={isDragActive ? 'true' : 'false'} {...getRootProps()}>
+          <input {...getInputProps()} />
+          <UploadFileIcon
+            sx={{
+              width: '25%',
+              height: '25%',
+            }}
+          />
+          <Typography mt={1} variant="h6" fontWeight={700} textAlign="center">
+            Select a CSV file to upload
+          </Typography>
+          <Typography
+            sx={{
+              opacity: 0.6,
+            }}
+            mt={0.5}
+          >
+            or drag and drop it here
+          </Typography>
+        </DropArea>
+      ) : (
+        <FileDataWrapper>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            maxWidth="80%"
+          >
+            <IconWrapper>
+              <InsertDriveFileIcon />
+            </IconWrapper>
+            <CustomTypography ml={2} variant="h5">
+              {file.name}
+            </CustomTypography>
+          </Box>
+          <div>
+            <IconButton
+              onClick={() => {
+                setFile(undefined);
+              }}
+              sx={{
+                marginRight: '16px',
+                width: '45px',
+                height: '45px',
+                borderRadius: '50%',
+              }}
+            >
+              <DeleteIcon
+                sx={{
+                  color: '#f44336',
+                  width: '32px',
+                  height: '32px',
+                }}
+              />
+            </IconButton>
+          </div>
+        </FileDataWrapper>
+      )}
+    </>
+  );
+};
+
+export default DragNDrop;
