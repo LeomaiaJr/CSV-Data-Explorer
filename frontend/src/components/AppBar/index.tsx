7ca71e740380e981d6e4750d4b678d9327562e33
LeomaiaJr <leomaiajr@hotmail.com>
2023-08-01 15:11:31 -0300

    feat: ad appbar component

---
+++ frontend/src/components/AppBar/index.tsx
@@ -0,0 +1,24 @@
+import { AppBar as MuiAppBar } from '@mui/material';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { FunctionComponent } from 'react';
+import ThemeSelector from '../ThemeSelector';
+import { Title, TitleWrapper } from './styles';
+
+const AppBar: FunctionComponent = () => {
+  return (
+    <MuiAppBar position="relative">
+      <Toolbar>
+        <TitleWrapper>
+          <Title variant="h6">CSV Data Explorer</Title>
+        </TitleWrapper>
+
+        <Box position="absolute" right={16}>
+          <ThemeSelector />
+        </Box>
+      </Toolbar>
+    </MuiAppBar>
+  );
+};
+
+export default AppBar;
